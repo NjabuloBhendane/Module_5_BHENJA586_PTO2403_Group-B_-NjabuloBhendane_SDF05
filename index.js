963cdadfc790b3373ed2fc910edbfae8c165e9cc
Njabulo Bhendane <njabulobhendane1996@gmail.com>
2024-04-19 20:04:42 +0200

    working through scrimba

--- index.js
+++ index.js
@@ -4,8 +4,22 @@
 // 2. Create a variable, sum, and set it to the sum of the two cards
 
 let firstCard = 6
-let secondCard = 9
-
+let secondCard = 10
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
 
-console.log(sum)
+function startgame() {
+    if (sum <= 20) {
+        message ="Do you want to draw a new card?"
+      } else if (summ===21){
+        message = "Wohoo! You've got Blackjack! "
+          hasBlackJack =true
+      } else {
+          message = "You're out of the game! "
+          isAlive = false
+      }
+      
+  console.log(message)
+}
